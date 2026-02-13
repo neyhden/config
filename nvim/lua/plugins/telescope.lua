d50f05f200a04aca9baedb5ad763b07640c1bbe5
Elias Hernandez Abreu <eherabr@contactel.com>
2026-02-13 09:55:32 +0000

    NVIM - keymap

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -4,7 +4,7 @@ return {
 		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
 			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "tf", builtin.find_files, {})
+			vim.keymap.set("n", "<C-f>", builtin.find_files, {})
             vim.keymap.set("n", "tb", ":Telescope buffers<CR>", {})
             require('telescope').setup{
                 defaults = {
