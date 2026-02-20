21e81e365214eaccdbf4cabdde16867be0e3d0fa
Elias Hernandez Abreu <eherabr@contactel.com>
2026-02-20 11:54:14 +0000

    NVIM hide dotfiles

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -11,7 +11,7 @@ return {
         filtered_items = {
           visible = false,
           show_hidden_count = true,
-          hide_dotfiles = false,
+          hide_dotfiles = true,
           hide_gitignored = true,
         },
         follow_current_file = {
