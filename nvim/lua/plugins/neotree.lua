4a04809e4694da7bd511689e1a50093498911ef9
Elias Hernandez Abreu <eherabr@contactel.com>
2026-02-20 11:53:10 +0000

    follow cursor NVIM

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -6,6 +6,21 @@ return {
     "MunifTanjim/nui.nvim",
   },
   config = function()
+    require("neo-tree").setup({
+      filesystem = {
+        filtered_items = {
+          visible = false,
+          show_hidden_count = true,
+          hide_dotfiles = false,
+          hide_gitignored = true,
+        },
+        follow_current_file = {
+          enabled = true,
+          leave_dirs_open = false,
+        },
+      },
+      buffers = { follow_current_file = { enable = true } },
+    })
     vim.keymap.set("n", "<leader>e", function()
       require("neo-tree.command").execute({
         toggle = false,
