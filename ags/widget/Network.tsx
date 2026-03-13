1ea5c157165a531b51ac5b97c28a621f91c8df47
Elias Hernandez Abreu <eherabr@contactel.com>
2026-03-13 08:40:37 +0000

    AGS - network uses primary

--- ags/widget/Network.tsx
+++ ags/widget/Network.tsx
@@ -1,15 +1,38 @@
 import AstalNetwork from "gi://AstalNetwork?version=0.1"
 import { Metric } from "./Metric"
-import { createBinding } from "gnim"
+import { createBinding, createEffect, createState } from "gnim"
 
 export const NetworkStatus = () => {
-  const network = AstalNetwork.get_default().wifi
+  const network = AstalNetwork.get_default()
+  const primary = createBinding(network, "primary")
+
+  const [label, setLabel] = createState("")
+  const [icon, setIcon] = createState("")
+  createEffect(() => {
+    switch (primary()) {
+      case AstalNetwork.Primary.WIFI: {
+        setLabel(network.wifi.ssid)
+        setIcon(network.wifi.iconName)
+        break;
+      }
+      case AstalNetwork.Primary.WIRED: {
+        setLabel("")
+        setIcon(network.wired.iconName)
+        break;
+      }
+      case AstalNetwork.Primary.UNKNOWN: {
+        setLabel("Unknown network")
+        setIcon("network-wireless-no-route-symbolic")
+        break;
+      }
+    }
+  })
 
   return (
     <Metric
       className="network-metric"
-      label={createBinding(network, "ssid")}
-      iconName={createBinding(network, "iconName")}
+      label={label}
+      iconName={icon}
     />
   )
 }
