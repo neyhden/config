610ce709d33401d71d6c503085296be9882cf645
Elias Hernandez Abreu <eherabr@contactel.com>
2026-04-28 08:07:44 +0100

    NVIM - git line blame delay

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -31,8 +31,8 @@ return {
       current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
       current_line_blame_opts = {
         virt_text = true,
-        virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
-        delay = 1000,
+        virt_text_pos = 'right_align', -- 'eol' | 'overlay' | 'right_align'
+        delay = 0,
         ignore_whitespace = false,
         virt_text_priority = 100,
         use_focus = true,
